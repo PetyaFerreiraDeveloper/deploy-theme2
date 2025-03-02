757fffcbaf718430b17b856ace400c33fada29e4
Petya Naydenova Ferreira <pn@abtion.com>
2025-03-02 11:29:23 +0100

    add a third paragraph

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 <h2 class="wrapper">the title is long</h2>
 <p class="parag">this is my first paragraph</p>
 <p class="parag">this is my second paragraph</p>
+<p class="red">this is my third paragraph</p>
 
 <?php get_footer() ?>
