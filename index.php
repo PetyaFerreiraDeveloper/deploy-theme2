254f4c7227135b62b3d733c05948b0fe8b255187
Petya Naydenova Ferreira <pn@abtion.com>
2025-03-02 11:09:50 +0100

    update paragraph

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 <h1 class="wrapper">Hello world</h1>
 <h2 class="wrapper">the title is long</h2>
 <p class="parag">this is my first paragraph</p>
+<p class="parag">this is my second paragraph</p>
 
 <?php get_footer() ?>
