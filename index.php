6f9febc57c80abe2c019424f6610fa7044afd18f
Petya Naydenova Ferreira <pn@abtion.com>
2025-03-05 20:35:09 +0100

    add styles

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 <h1 class="wrapper">Hello world</h1>
 <h2 class="wrapper">the title is long</h2>
-<p class="parag">this is my first paragraph</p>
-<p class="parag">this is my second paragraph</p>
-<p class="red">this is my third paragraph</p>
+<div class="container">
+  <p class="parag">this is my first paragraph</p>
+  <p class="parag">this is my second paragraph</p>
+  <p class="red">this is my third paragraph</p>
+</div>
 <p class="red">this is my fourth paragraph</p>
 <p class="blue">this is my fifth paragraph</p>
 <p class="orange">this is my sixth paragraph</p>
